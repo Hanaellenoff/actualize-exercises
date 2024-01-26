2dd9c432da919d1eb9114b37e12593b806102ef5
Hanaellenoff <hanaellenoff@gmail.com>
2024-01-26 11:47:01 -0500

    Create 121.rb

---
+++ 121.rb
@@ -0,0 +1,13 @@
+# QUESTION 1
+# Write example code to demonstrate three string methods you've never 
+# worked with before from the Ruby documentation. 
+# Write a comment for each method describing how it works in your own words.
+meow = "cat"
+p meow.clear # this clears the string you put leaving an empty space
+lorem = "ipsum" 
+p lorem.concat("di", "ablo") # this concatenates the strings together by putting more strings in the parenthesis 
+p "balloon".delete "l" # this will delete the letters you tell it to
+# QUESTION 2
+# Write example code that crashes using an integer method from the Ruby documentation. 
+# Write a comment describing the error message in your own words.
+p 36.gcdlcm # it doesnt recognize this method I need to add a number after the method
