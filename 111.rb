9e77451e6fb06ba0cee9bfc75ee673c3bdd358b0
Hanaellenoff <hanaellenoff@gmail.com>
2024-01-25 19:55:41 -0500

    Create 111.rb

---
+++ 111.rb
@@ -0,0 +1,16 @@
+# QUESTION 1
+# Write code to print out the numbers 4, 30, 12, 983 on separate lines. 
+# For this exercise, you are required to use addition, subtraction, multiplication, and division at least once.
+p 10 - 6
+p 5 * 6
+p 144 / 12
+p 982 + 1
+
+# QUESTION 2
+# Write code using addition, subtraction, multiplication, or division with numbers that causes the computer to crash.
+# Add a comment with a copy of the error message.
+# Add a comment that describes the error message in your own words.
+
+p - 12 + 89 
+# 111.rb:14:in `<main>': undefined method `-' for nil:NilClass (NoMethodError)
+# It doesnt recognize the negative symbol before 12 (-) Theres also a space between the 12 and the sign
