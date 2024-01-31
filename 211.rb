487b9d520751c30d6649c1314082f760fec5a06c
Hana Ellenoff <hanaellenoff@gmail.com>
2024-01-31 17:26:06 -0600

    Add practice problem

---
+++ 211.rb
@@ -0,0 +1,14 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+# Write a while loop that prints the phrase "Around the world" 144 times.
+number = 0
+while number < 144
+  puts "around the world"
+  number = number + 1 
+end
+# Were you able to easily solve the problem from memory? for the most part but I just forgot to use a 0 instead of 1 and <=
+# If so, describe the next skill you will start to practice tomorrow. I want to continue loops to make sure I have it downpact but use a harder loop 
+# If not, describe the part you got stuck on that requires more practice.
+
+
