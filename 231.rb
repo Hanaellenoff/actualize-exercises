e80dd1a50f5c968f9db5ffb1bd69f85b0ca00e2a
Student <Student@Admins-MacBook-Air.local>
2024-02-01 11:45:35 -0600

    Add exercise solutions

---
+++ 231.rb
@@ -0,0 +1,14 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.# Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
+qwerty = [4,2,66,24,71]
+index = 0 
+while index < qwerty.length
+puts qwerty[index]
+  index = index + 1
+end
+# Were you able to easily solve the problem from memory? yes 
+# If so, describe the next skill you will start to practice tomorrow. more and harder arrays 
+# If not, describe the part you got stuck on that requires more practice.
+
+
