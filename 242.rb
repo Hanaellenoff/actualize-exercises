8dc1fc79abae2fbb608e903e4842f932c040dee2
Hana Ellenoff <hanaellenoff@gmail.com>
2024-02-01 18:38:14 -0600

    Add practice problem

---
+++ 242.rb
@@ -0,0 +1,11 @@
+# QUESTION 1
+# Use a variable to store an array with three different words.
+# Then add one more word to the array and print the array on one line.
+qwerty = ["weeoo", "asdf", "red"]
+qwerty << "meat"
+pp qwerty
+# QUESTION 2
+# Use a variable to store a hash with two different words and their definitions.
+# Then add one more word with its definition and print the hash on one line.
+asdf = {"cash" => "money", "taxes" => "annoying"}
+asdf ["fish" => "animal that swims"]
