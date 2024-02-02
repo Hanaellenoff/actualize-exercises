1d76ef01ba1d74bcb7c56095fbd2fe0026f4021c
Student <hanaellenoff@gmail.com>
2024-02-02 15:50:34 -0600

    Create 251.rb

---
+++ 251.rb
@@ -0,0 +1,15 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+# Make a hash to store prices for 3 different menu items. Then add a new menu item and price and print the hash to see the result.
+menu = {"pizza" => "5.00", "soup" => "3.99", "salad" => 4.50}
+menu["fries"] = 3.00
+pp menu
+
+
+
+# Were you able to easily solve the problem from memory? yes
+# If so, describe the next skill you will start to practice tomorrow. I wanna go back to work on loops i think i need more practice
+# If not, describe the part you got stuck on that requires more practice.
+
+
