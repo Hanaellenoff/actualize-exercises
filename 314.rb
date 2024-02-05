275b16ae3e51ab01bda4057427fe28aa987aea01
Student <hanaellenoff@gmail.com>
2024-02-04 22:38:20 -0600

    Add exercise solutions

---
+++ 314.rb
@@ -0,0 +1,59 @@
+# Step 1: Read the problem below.
+# Step 2: Decompose the problem into smaller testable problems. DO NOT SOLVE THE PROBLEM YET.
+# Step 3: Solve each problem until you get stuck. YOU DO NOT NEED TO SOLVE THE COMPLETE PROBLEM!
+
+
+# QUESTION 1
+# Ancay ouyay igurefay histay neoay utoay?
+# Pig Latin is a very sophisticated language in which 
+# each English word is converted as follows: 
+# The first letter of each word is removed from the beginning and added to the end. 
+# Then, the letters 'ay' are added to the very end of the word.
+# For example: hello = ellohay, pingpong = ingpongpay, marmalade = armalademay, etc.
+# Write a program that asks the user to enter a single word 
+# and prints out the Pig Latin version of that word.
+
+# ask the user to enter a word then print it
+# puts "please type a word:"
+# word = gets.chomp
+# puts  word.split
+
+# make a new string
+# puts "please type a word:"
+# word = gets.chomp
+# puts word 
+# new_string = "ay"
+# puts new_string
+
+# combind a string with a word
+# new_string = "ay"
+# word = "meow" 
+# puts word + new_string
+
+# split a String
+# word = "meow"
+# p word.split(//)
+# p word.length
+
+# add something to the letters in the Array
+# word = "meow"
+# p word.split(//)
+# new_array = word.split(//)
+# index = 0
+# while index < word.length
+#   puts new_array[index] + "ay"
+#   index = index + 1
+# end
+
+# call on the first letter of the array/ word 
+#  word = "meow"
+# p word.split(//)
+# new_array = word.split(//)
+# p new_array[0] 
+
+# add the first letter to the end of the word
+word = "meow"
+p word.split(//)
+new_array = word.split(//)
+p new_array[0] 
+# i got stuck here
---
+++ 321.rb
@@ -0,0 +1,17 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+# Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
+
+index = 0 
+while index <= 100
+puts index 
+index = index + 5
+end  
+
+# Were you able to easily solve the problem from memory? no
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+# i got confused by what it was asking and put times 5 instead of plus idk how to practice fixing that....
+
