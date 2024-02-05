c4059e9747747da0730f9bd51ee1e6ebde2421f3
Student <hanaellenoff@gmail.com>
2024-02-04 19:33:17 -0600

    Create 311.rb

---
+++ 311.rb
@@ -0,0 +1,14 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+# Write a program that uses variables to store the names of three cities, then prints out a sentence using that information with string concatenation (the + operator).
+g = "atlanta"
+i = "chicago"
+m = "sharon"
+puts "I currently live in " + g + " but am dorming in " + i + " my cousins live in " + m
+# Were you able to easily solve the problem from memory? yes 
+# If so, describe the next skill you will start to practice tomorrow. more on loops still need that practice
+# If not, describe the part you got stuck on that requires more practice.
+
+
