10de6aac61f8c474804912012fe4d968c5a8ed21
Student <hanaellenoff@gmail.com>
2024-02-13 14:46:08 -0600

    Add exercise solutions

--- 442.rb
+++ 442.rb
@@ -1,17 +1,20 @@
 # QUESTION 1
 # Write a method that takes in an array of numbers and returns an array with every number doubled.
 # Then run the method and print the result.
+# See if you can invert this and put your loop inside your method or have your loop call your method versus trigger it. Let me know if you need more clarity on this.
+
+def doubled(input_a)
+  index = 0
+  new_array = []
+  while index < input_a.length
+    number = input_a[index]
+    two = number * 2
+    new_array.push(two)
+    index += 1
+  end 
+  return new_array
+end 
+
 
 numbers = [2,3,6,1]
-index = 0 
-new_array = []
-while index < numbers.length
-  number = numbers[index]
-  def doubled(number) 
-    return number * 2 
-  end 
-  two = doubled(number)
-  new_array.push(two)
-  index += 1
-end 
-p new_array
+p doubled(numbers)
